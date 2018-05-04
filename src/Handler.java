70112bae4bed2fa1a4de245ac95e33b111a79450
cluter123 <guangze.zu@gmail.com>
2018-05-04 13:54:04 -0500

    Conor Code Merge

--- src/Handler.java
+++ src/Handler.java
@@ -1,42 +1,118 @@
+package Main;
+
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.util.LinkedList;
 
+/**
+ * @author Conor Mai
+ *
+ */
 public class Handler {
 
-	private LinkedList<Character> objects = new LinkedList<Character>();
+	public LinkedList<Character> objects;
 	
-	public Handler() {
+	/** Creates a linked list of characters
+	 * 
+	 */
+	public Handler() 
+	{
 		// TODO Auto-generated constructor stub
+		objects = new LinkedList<Character>();
 	}
 	
+	/**Goes through all of the Characters and updates them
+	 * 
+	 */
 	public void update()
 	{
-		for(int k = 0; k < objects.size(); k++)
+		for(Character tempCharacter : objects)
 		{
-			Character tempObject = objects.get(k);
-			
-			tempObject.update();
+			tempCharacter.update();
 		}
 	}
 	
-	public void render(Graphics2D g)
+	/**Goes through all of the Characters and makes them draw themselves
+	 * @param g
+	 */
+	public void draw(Graphics g)
 	{
-		for(int k = 0; k < objects.size(); k++)
+		for(Character tempCharacter : objects)
 		{
-			Character tempObject = objects.get(k);
-			
-			tempObject.render(g);
+			tempCharacter.draw(g);
 		}
 	}
 	
+	/**Adds an object to the list of characters
+	 * @param object
+	 */
 	public void addObject(Character object)
 	{
 		objects.add(object);
 	}
 	
+	/**removes an object from the list of characters
+	 * @param object
+	 */
 	public void removeObject(Character object)
 	{
 		objects.remove(object);
+	}
+	
+	/**Goes through the collection of characters and finds the player 1 
+	 * and makes it move to the left
+	 */
+	public void moveP1Left()
+	{
+		for(Character tempCharacter : objects)
+		{
+			if(tempCharacter.getId() == 1)
+			{
+				tempCharacter.setVelX(-5);
+			}
+		}
+	}
+	
+	/**Goes through the collection of characters and finds the player 1 
+	 * and makes it move to the right
+	 */
+	public void moveP1Right()
+	{
+		for(Character tempCharacter : objects)
+		{
+			if(tempCharacter.getId() == 1)
+			{
+				tempCharacter.setVelX(5);
+			}
+		}
+	}
+	
+	/**Goes through the collection of characters and finds the player 1 
+	 * and stops it if it is moving to the left
+	 */
+	public void stopP1Left() 
+	{
+		for(Character tempCharacter : objects)
+		{
+			if(tempCharacter.getId() == 1)
+			{
+				if(tempCharacter.getVelX() < 0)
+					tempCharacter.setVelX(0);
+			}
+		}
+	}
+	
+	/**Goes through the collection of characters and finds the player 1 
+	 * and stops it if it is moving to the right
+	 */
+	public void stopP1Right()
+	{
+		for(Character tempCharacter : objects)
+		{
+			if(tempCharacter.getId() == 1)
+			{
+				if(tempCharacter.getVelX() > 0)
+					tempCharacter.setVelX(0);
+			}
+		}
 	}
 }
--- src/MapViewer.java
+++ src/MapViewer.java
@@ -1,23 +1,71 @@
+package Main;
+
 import java.awt.Canvas;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.Timer;
 
-public class MapViewer extends Canvas {
+public class MapViewer extends Canvas{
 
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6818686894060817625L;
+	private static Timer drawTimer;
+	/**Creates a new frame with the given width height and game
+	 * @param width
+	 * @param height
+	 * @param title
+	 * @param game
+	 */
 	public MapViewer(int width, int height, String title, Map game) 
 	{
 		JFrame frame = new JFrame(title);
+		
+		//makes a new frame with all of the given dimension
 		frame.setPreferredSize(new Dimension(width, height));
 		frame.setMaximumSize(new Dimension(width, height));
 		frame.setMinimumSize(new Dimension(width, height));
 		
+		//makes sure that it closes when we want it to
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//can't change the size of the frame
 		frame.setResizable(false);
+		
+		//starts at the center of the screen 
+		//(because its the most important thing that is happening)
 		frame.setLocationRelativeTo(null);
+		
+		//adds the game to the frame
 		frame.add(game);
+		
+		//makes it so that we can see it
 		frame.setVisible(true);
-		game.start();
+		
+//		//starts the game
+//		game.start();
+		class AdvanceTimerListener implements ActionListener
+		{
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				drawTimer.stop();
+				game.update();
+				drawTimer.start();
+				game.draw();
+			}
+		}
+		
+		ActionListener advanceListener = new AdvanceTimerListener();
+		
+		final int FPS = 60; // Milliseconds between timer ticks
+		drawTimer = new Timer(1000/ FPS, advanceListener);
+		drawTimer.start();
 	}
 
 }
